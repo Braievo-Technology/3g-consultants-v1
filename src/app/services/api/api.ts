db152bc158370d7837cbab9baf2ebc6c97a1a62d
Kaveeja Sachintha <kaveejasperera@gmail.com>
2025-05-16 02:56:57 +0530

    fix the cta button section in careerspage

--- src/app/services/api/api.ts
+++ src/app/services/api/api.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: "http://localhost:3000/api",
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 export default api;
