086f150703d7a9f46a97463ea694e5a61f18640d
likshan-lahiru <lahiru212001@gmail.com>
2025-05-15 22:23:42 +0530

    change base url get it env file

--- src/app/api/event/[id]/route.ts
+++ src/app/api/event/[id]/route.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-nocheck
+
 import { NextRequest, NextResponse  } from 'next/server';
 import { PrismaClient  } from '@prisma/client';
 import {uploadToAzureBlob} from "@/app/api/util/blobService";
