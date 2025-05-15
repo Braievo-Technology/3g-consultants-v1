c3c35026eb2be8aec82822840a36ba2d2460a361
Kaveeja Sachintha <kaveejasperera@gmail.com>
2025-05-16 03:01:29 +0530

    fix the job application

--- src/app/jobapplication/[id]/page.tsx
+++ src/app/jobapplication/[id]/page.tsx
@@ -368,19 +368,6 @@ const JobApplicationPage = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <div>
-              <label className="block text-gray-700 mb-2">
-                Expected Salary *
-              </label>
-              <input
-                type="text"
-                name="expectedSalary"
-                value={formData.expectedSalary}
-                onChange={handleInputChange}
-                required
-                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              />
-            </div>
           </div>
         );
       case 3:
